e1e236207cab0230fdc8f2da90e662735ecfb35c
Dasun Pubudumal <dasun.15@cse.mrt.ac.lk>
2017-12-20 07:00:07 +0530

    "Written by implemented."

--- app/Post.php
+++ app/Post.php
@@ -14,4 +14,10 @@ class Post extends Model
 
     //Timestamps
     public $timestamps = true;
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
+
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,6 +15,32 @@
                     @endif
                         <a href="/posts/create" class="btn btn-primary">Create Posts</a>
                     <h3>Your Blog Posts</h3>
+
+                    <table class="table table-striped">
+
+                        @if(count($posts) > 0)
+                        <tr>
+                            <th>Title</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                        @foreach($posts as $post)
+                            <tr>
+                                <td>{{$post->title}}</td>
+                                <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></td>
+                                <td>
+                                    {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])  !!}
+                                    {{Form::hidden('_method', 'DELETE')}}
+                                    {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                                    {!! Form::close() !!}
+                                </td>
+                            </tr>
+                        @endforeach
+                        @else
+                            <p>No posts found.</p>
+                        @endif
+
+                    </table>
                 </div>
             </div>
         </div>
